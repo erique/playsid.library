23745dcbfab2a978133ae8f40a5d3c4accab510f
K-P Koljonen <kpkoljonen@gmail.com>
2023-03-06 21:30:01 +0200

    Remove debug env flag reading, seems to cause random crashes

--- playsid.asm
+++ playsid.asm
@@ -464,9 +464,13 @@ SetDefaultOperatingMode:
 
 
 GetEnvDebugFlag:
+ REM
+        DPRINT  "GetEnvDebugFlag"
         lea     -20(sp),sp
         move.l  a6,a5
         move.l  psb_DOSBase(a5),a6
+        move.l  a6,d0
+        DPRINT  "dos=%lx"
         cmp     #36,LIB_VERSION(a6)
         blo     .continue
 
@@ -490,7 +494,8 @@ GetEnvDebugFlag:
 .envVarNameDebug:
     dc.b    "PlaySIDDebug",0
     even
-
+ EREM
+ 
 *-----------------------------------------------------------------------*
 @FreeEmulResource
         DPRINT  "FreeEmulResource"
