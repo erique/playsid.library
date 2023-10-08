bbea1b1d206d3193900bce29a420ecc8bc07100a
Erik Hemming <eriquesnk@gmail.com>
2023-10-08 20:22:00 +0200

    mark /host within container as git-safe.

--- docker.sh
+++ docker.sh
@@ -5,4 +5,4 @@
 # $ ./docker.sh "echo | m68k-amigaos-gcc -dM -E -" | grep __VERSION__
 
 # Use a published container
-docker run --volume "$PWD":/host --workdir /host -i -t trixitron/m68k-amigaos-gcc /bin/bash -c "$*"
+docker run --volume "$PWD":/host --workdir /host -i -t trixitron/m68k-amigaos-gcc /bin/bash -c "git config --global --add safe.directory /host && $*"
