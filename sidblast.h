48fc524adac457e9fd0858daa05c8d3b0b570b68
Erik Hemming <eriquesnk@gmail.com>
2025-01-28 00:13:25 +0100

    sid_reset()

--- sidblast.h
+++ sidblast.h
@@ -6,3 +6,4 @@ uint8_t sidblaster_read_reg(register uint8_t reg __asm("d0"));
 void    sidblaster_write_reg(register uint8_t reg __asm("d0"), register uint8_t value __asm("d1"));
 void    sidblaster_write_reg_record(register uint8_t reg __asm("d0"), register uint8_t value __asm("d1"));
 void    sidblaster_write_reg_playback();
+void    sidblaster_reset();
